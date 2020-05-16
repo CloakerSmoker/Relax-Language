a1512e9fe85e901e2e2f6ff2d3b04e3ce3dcb803
CloakerSmoker <cloakersmoker@users.noreply.github.com>
2020-05-15 22:29:43 -0500

    Added support for nested structs, changed test_bootstrap to test output executables recursively (on themselves), added a missing error message for using the '.' operator on a struct pointer

--- test_bootstrap.py
+++ test_bootstrap.py
@@ -16,27 +16,41 @@ init(autoreset=True)
 cwd = os.getcwd()
 path_join = os.path.join
 
+recursion_count = 3
+
+if len(sys.argv) == 2:
+    recursion_count = int(sys.argv[1])
+
 safe_compiler = path_join(cwd, 'stable_version.exe')
-compile_command = f'{safe_compiler} Bain.rlx testing.exe'
-compiler_output = path_join(cwd, 'testing.exe')
 
-compile_result = subprocess.run(compile_command, cwd=cwd, shell=True, capture_output=True)
-stderr_text = compile_result.stderr.decode('UTF-8')
+for i in range(0, recursion_count):
+    compile_command = f'{safe_compiler} Bain.rlx testing{i}.exe'
+    compiler_output = path_join(cwd, f'testing{i}.exe')
 
-if compile_result.returncode != 1 or len(stderr_text) != 0:
-    print(f'{Fore.LIGHTRED_EX}Compile error ({hex(compile_result.returncode)}):\n{stderr_text}', file=sys.stderr)
-    sys.exit(1)
+    compile_result = subprocess.run(compile_command, cwd=cwd, shell=True, capture_output=True)
 
-test_script = path_join(cwd, 'test_compiler.py')
-test_command = f'python {test_script} {compiler_output}'
+    if i != 0:
+        os.remove(safe_compiler)
 
-test_result = subprocess.run(test_command, cwd=cwd, shell=True, capture_output=True)
-stderr_text = test_result.stderr.decode('UTF-8')
+    stderr_text = compile_result.stderr.decode('UTF-8')
 
-if len(stderr_text):
-    print(f'{Fore.LIGHTRED_EX}Output file failed 1+ test:\n{stderr_text}', file=sys.stderr)
-    os.remove(compiler_output)
-    sys.exit(1)
+    if compile_result.returncode != 1 or len(stderr_text) != 0:
+        print(f'{Fore.LIGHTRED_EX}Compile error ({hex(compile_result.returncode)}):\n{stderr_text}', file=sys.stderr)
+        sys.exit(1)
 
-print(f'{Fore.LIGHTGREEN_EX}Output file passed all tests.')
+    test_script = path_join(cwd, 'test_compiler.py')
+    test_command = f'python {test_script} {compiler_output}'
+
+    test_result = subprocess.run(test_command, cwd=cwd, shell=True, capture_output=True)
+    stderr_text = test_result.stderr.decode('UTF-8')
+
+    if len(stderr_text):
+        print(f'{Fore.LIGHTRED_EX}Output file iteration {i + 1} failed 1+ test:\n{stderr_text}', file=sys.stderr)
+        os.remove(compiler_output)
+        sys.exit(1)
+    
+    print(f'{Fore.LIGHTGREEN_EX}Output file iteration {i + 1} passed all tests.')
+    safe_compiler = compiler_output
+
+print(f'{Fore.LIGHTGREEN_EX}Output file(s) passed all tests.')
 move(compiler_output, 'new_stable.exe')
